38494615c104d8a7fc6de68dbc33f1a01d11684d
aidaningram <aidaningram.video@gmail.com>
2026-06-11 08:52:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,8 @@ window.addEventListener("scroll", () => {
     const heroHeight = window.innerHeight;
 
     if (window.scrollY > heroHeight - 100) {
-        navbar.classList.add("hidden");
+        navbar.classList.add("compact");
     } else {
-        navbar.classList.remove("hidden");
-    }
-});
-
-document.addEventListener("mousemove", (event) => {
-    if (event.clientY < 80) {
-        navbar.classList.remove("hidden");
-    } else if (window.scrollY > window.innerHeight - 100) {
-        navbar.classList.add("hidden");
+        navbar.classList.remove("compact");
     }
 });
