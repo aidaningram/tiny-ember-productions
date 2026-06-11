39524ec600fe0ece81eb37a261ab8f627c8a69fd
aidaningram <aidaningram.video@gmail.com>
2026-06-10 23:34:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("Tiny Ember Productions website loaded.");
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    const heroHeight = window.innerHeight;
+
+    if (window.scrollY > heroHeight - 100) {
+        navbar.classList.add("hidden");
+    } else {
+        navbar.classList.remove("hidden");
+    }
+});
+
+document.addEventListener("mousemove", (event) => {
+    if (event.clientY < 80) {
+        navbar.classList.remove("hidden");
+    } else if (window.scrollY > window.innerHeight - 100) {
+        navbar.classList.add("hidden");
+    }
+});
